636b8529f0c290fbf88fe11ac0dafa5ce9a0edf9
Pasta <Tmartins2008@icloud.com>
2024-04-18 13:21:38 -0400

    Check

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,82 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgLogo;
+  PImage imgBackg;
+
+  float fltLogoX = 150;
+  float fltLogoY = 300;
+  int intMoveX = 5;
+
+  float fltSquareX = 150;
+  float fltSquareY = 0;
+  float fltSquareMoveX = 10;
+  float fltSquareMoveY = (float)7.5;
+
+  float fltCircleDistanceX;
+  float fltCircleDistanceY;
+  
+  
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(40, 70, 100);
+
+    imgBackg = loadImage("/images/Radiance.jpg");
+    imgLogo = loadImage("/images/Logo.png");
+    imgLogo.resize(width / 4, height / 4);
+    imgBackg.resize((int)(imgBackg.width/3.6), (int)(imgBackg.height/3.6));
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // background
+    image(imgBackg, -240, 0);
+
+    // everything involving the logo
+    image(imgLogo, fltLogoX, fltLogoY);
+
+    fltLogoX += intMoveX;
+    fltLogoY = 50 * (float)Math.sin(5 * (fltLogoX + 5)) + 120;
+
+    if (fltLogoX < 0 + 75 || fltLogoX > width - imgLogo.width - 75) {
+      intMoveX *= -1;
+    }
+    
+    // shape 
+    stroke(54, 54, 105);
+    fill(93, 138, 168);
+    rect(fltSquareX, fltSquareY, width/8, height/8);
+
+    fltSquareX += fltSquareMoveX;
+    fltSquareY += fltSquareMoveY;
+
+    if (fltSquareX < 0 || fltSquareX > width - width/8) {
+      fltSquareMoveX *= -1;
+    }
+
+    if (fltSquareY < 0 || fltSquareY > height - height/8) {
+      fltSquareMoveY *= -1;
+    }
+
+    // shape collision (THIS TOOK SO MUCH PAIN AND SUFFERING)
+    fltCircleDistanceX = abs(fltLogoX + 25 - fltSquareX);
+    fltCircleDistanceY = abs(fltLogoY + 25 - fltSquareY);
+
+    if (fltCircleDistanceX <= (width / 8 / 2 + width / 8) && fltCircleDistanceY <= (height / 8 / 2 + height / 8)) { 
+      if (fltCircleDistanceX - 10.5 >= (width / 8 / 2 + width / 8) || fltCircleDistanceX + 10.5 >= (width / 8 / 2 + width / 8)){
+        fltSquareMoveX *= -1; 
+        fltSquareX += fltSquareMoveX;
+      } else {
+        fltSquareMoveY *= -1;
+        fltSquareY += fltSquareMoveY;
+      }
+    }
+
   }
-  
-  // define other methods down here.
+
 }
